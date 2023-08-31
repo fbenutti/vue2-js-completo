95a364ca6de80f382282b36df0339c86ac569374
Felipe Benutti <felipe.benutti@privacy.com.br>
2023-08-30 22:23:47 -0300

    Section 7 class 113

---
+++ exemplos-iniciais/componentes/componente-desafio/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <side-drawer/>
+    <nav-bar/>
+    <carousel-hero />
+    <app-footer/>
+  </v-app>
+</template>
+
+<script>
+import SideDrawer from './components/SideDrawer.vue'
+import NavBar from './components/NavBar.vue'
+import Carousel from './components/Carousel.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    'side-drawer': SideDrawer,
+    'nav-bar': NavBar,
+    'carousel-hero': Carousel,
+    'app-footer': Footer
+  }
+}
+</script>
---
+++ exemplos-iniciais/componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="contadores">
+    <h1> CONTADORES </h1>
+    <app-contador v-for="c in 5" :key="c"/>
+  </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+export default {
+    components: { 'app-contador': Contador }
+}
+</script>
+
+<style>
+
+</style>
+
+<!-- <style scoped>
+    div {
+        border: 1px solid red;
+    }
+</style> -->
